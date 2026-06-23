8696c5fcf470eda5ebbc8cea8263869f961c448d
kongsuper <3483645078@qq.com>
2026-06-23 11:19:30 +0800

    fix: simplify batch script to avoid encoding issues

--- start.bat
+++ start.bat
@@ -1,18 +1,15 @@
 @echo off
-chcp 65001 >nul
 title 数字收藏馆
 
-cd /d "%~dp0"
+cd /d "G:\Blog"
 
 if not exist "node_modules" (
-    echo [1/2] 检测到未安装依赖，正在安装...
+    echo Installing dependencies...
     call npm install
-    echo.
 )
 
-echo [2/2] 启动开发服务器...
-echo 浏览器打开 http://localhost:3000
-echo 按 Ctrl+C 停止
-echo.
-call npm run dev
+echo Starting dev server...
+echo Open http://localhost:3000
+echo Press Ctrl+C to stop
+npm run dev
 pause
